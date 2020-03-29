33c75778bf206dfa2601866ad4ff560c5a01add9
baktov <gael.henry@free.fr>
2020-03-29 14:11:13 +0200

    clés 0.85, ajout compendium marchandise, prise en compte de sdistance et des poids par babele (dans les compendiums) , wip sur items

--- dnd5e_fr-FR/babele-register.js
+++ dnd5e_fr-FR/babele-register.js
@@ -1,11 +1,33 @@
 
-Hooks.on('init', () => {
+Hooks.once('init', () => {
 
 	if(typeof Babele !== 'undefined') {
+		
 		Babele.get().register({
 			module: 'dnd5e_fr-FR',
 			lang: 'fr',
 			dir: 'compendium'
-		}); 
+		});
+
+		Babele.get().registerConverters({
+			"weight": (value) => { return parseInt(value)/2 },
+			"range": (range) => {
+				if(range) {
+					if(range.units === 'ft') {
+						if(range.long) {
+							range = mergeObject(range, { long: range.long*0.3 });
+						}
+						return mergeObject(range, { value: range.value*0.3 });
+					}
+					if(range.units === 'mi') {
+						if(range.long) {
+							range = mergeObject(range, { long: range.long*1.5 });
+						}
+						return mergeObject(range, { value: range.value*1.5 });
+					}
+					return range;
+				}
+			}
+		});
 	}
 });
